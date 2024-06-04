65f3a868ed8c5d8e20e5b2e03065cf7d46bd1af5
kasarci <muhammedkasarci@hotmail.com>
2024-06-04 15:02:47 +0200

    Added comment about range keyword.

--- arrays/sum.go
+++ arrays/sum.go
@@ -11,6 +11,9 @@ func SumAll(numbersToSum ...[]int) []int {
 	var sums []int
 
 	for _, numbers := range numbersToSum {
+		// range keyword lets you iterate over an array.
+		// On each iteration, range returns two values - the index and the value. 
+		// We are choosing to ignore the index value by using _ blank identifier.
 		sums = append(sums, Sum(numbers))
 	}
 	return sums
